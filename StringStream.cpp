22bdd86651e7fc90f795e28ccda8051648a2e51f
RawanShareef <73065095+rawanshareef@users.noreply.github.com>
2021-07-13 01:17:47 +0300

    Update StringStream.cpp

--- StringStream.cpp
+++ StringStream.cpp
@@ -1,10 +1,20 @@
+  
 #include <sstream>
 #include <vector>
 #include <iostream>
 using namespace std;
 
 vector<int> parseInts(string str) {
-	// Complete this function
+    // Complete this function
+     vector<int> my_vec;
+     stringstream ss(str);
+     char ch= ','; 
+     int a;
+     while(ss>>a){
+            my_vec.push_back(a);
+            ss>>ch;
+      }
+return my_vec;
 }
 
 int main() {
